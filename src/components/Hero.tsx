32585dc19c487436268f437da892b729cfba49a8
kasahun welela <getasewmequanint@cbe.com.et>
2024-06-13 16:10:27 +0300

    create hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+function Hero(props: { title: string; description: string }) {
+  return (
+    <div className='bg-[url("hero.png")] h-[40vh] flex flex-col items-center justify-center text-white gap-5'>
+      <p className="text-4xl md:text-5xl font-bold ">{props.title}</p>
+      <p className="max-w-sm text-center text-xl">{props.description}</p>
+    </div>
+  );
+}
+
+export default Hero;
