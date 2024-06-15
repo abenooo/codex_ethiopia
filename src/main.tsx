73c18d240ab91bcf2f7f8edb24b13abf771b438b
Abenezer <abenezerkifle000@gmail.com>
2024-06-15 05:51:43 -0700

    Use technology page

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import "./index.css";
 import "remixicon/fonts/remixicon.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
-
+import Technology from "./pages/Technology.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
+          <Route path="/technology" element ={< Technology/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
