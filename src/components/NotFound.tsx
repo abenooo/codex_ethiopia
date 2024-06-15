f53a821f4aba0733aa086ad3811817ada5840c87
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-15 13:26:27 +0300

    update text and look

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 function NotFound() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-5">
-      <p className="text-6xl font-bold text-primary">404</p>
-      <p className="font-semibold">UH You're lost</p>
-      <p className="max-w-sm text-center">
-        The page you are looking for doesn't exist how you got here is
-        mystery.But you can click the button below to go back to the homepage
+      <p className="text-6xl font-extrabold text-primary">404</p>
+      <p className="font-bold text-primary">Oops! Page not found</p>
+      <p className="max-w-sm text-center font-medium">
+        The page you are looking for might have been removed or temporarily
+        unavailable.
       </p>
       <Link className="px-4 py-2 bg-primary text-white rounded-md" to="/">
-        Home
+        Back to Home Page
       </Link>
     </div>
   );
