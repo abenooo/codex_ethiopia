dc6aa88316c9cc109b99709812af253f43ecaa61
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-24 09:49:42 +0100

    add hero section

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,7 +1,12 @@
-import TechnologyComponet from "../components/Technology"
+import Hero from "@/components/Hero";
+import TechnologyComponet from "../components/Technology";
 const Technology = () => {
   return (
     <div>
+      <Hero
+        title="Technology"
+        description="We specialize in helping you build a team of expert developers."
+      />
       <TechnologyComponet />
     </div>
   );
