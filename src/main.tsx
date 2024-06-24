7a152f2b1c875571789f6d221d2a7a51df7ecd63
kasahun-welela <kasahunwelela1@gmail.com>
2024-06-24 09:49:04 +0100

    update path to get pages

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import Technology from "./pages/Technology.tsx";
 import NotFound from "./components/NotFound.tsx";
-import Price from "./components/Pricing.tsx";
+import Pricing from "./pages/pricing.tsx";
 import Chatbot from "./components/Chatbot.tsx";
-import About from "./components/About.tsx";
+import About from"./pages/about.tsx"
 import Login from "./pages/login.tsx";
 import ForgotPassword from "./pages/ForgotPassword.tsx";
 import Blog from "./pages/Blog.tsx";
@@ -21,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="/technology" element ={< Technology/>}/>
-          <Route path="/pricing" element ={< Price/>}/>
+          <Route path="/pricing" element ={< Pricing/>}/>
           <Route path="/about" element ={< About/>}/>
           <Route path="/login" element ={< Login/>}/>
           <Route path="/forgot_password" element ={< ForgotPassword/>}/>
