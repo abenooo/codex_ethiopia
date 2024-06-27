23707108cb3367916ada72fd713813915433e825
Abenezer <abenezerkifle000@gmail.com>
2024-06-27 04:39:18 -0700

    Fix build error

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,14 +46,6 @@ const automationSubMenu: SubMenuItem[] = [
   { href: "/ranorex", title: "Ranorex" },
 ];
 
-const technologySubMenu: SubMenuItem[] = [
-  { href: "/backend", title: "Backend" },
-  { href: "/blockchain", title: "Blockchain" },
-  { href: "/cloud-computing", title: "Cloud Computing" },
-  { href: "/cyber-security", title: "Cyber Security" },
-  { href: "/iot", title: "Internet of Things" },
-  { href: "/machine-learning", title: "Machine Learning" },
-];
 
 export default function Navbar() {
   const location = useLocation();
@@ -546,27 +538,6 @@ function MenuIcon(props: React.SVGProps<SVGSVGElement>) {
   );
 }
 
-function PackageIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m7.5 4.27 9 5.15" />
-      <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z" />
-      <path d="m3.3 7 8.7 5 8.7-5" />
-      <path d="M12 22V12" />
-    </svg>
-  );
-}
 
 function UserIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
